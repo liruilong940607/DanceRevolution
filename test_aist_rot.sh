1fba2c2a361fe382fa33e0d8a8cf95d3a5a495fb
Ruilong Li <ruilongl@usc.edu>
2021-03-10 16:04:08 -0800

    update

--- test_aist_rot.sh
+++ test_aist_rot.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 
 # Generate pose sequences of json format and visualize them
-python test.py --input_dir data_aist_rot/test \
-               --model 60FPS_aist_rot/epoch_60.pt \
-               --json_dir 60FPS_aist_rot_outputs/ \
+python test.py --input_dir data_aist_rot/train \
+               --model 60FPS_aist_rot_v2/epoch_80.pt \
+               --json_dir 60FPS_aist_rot_outputs_v2/ \
                --batch_size 1 \
                --aist \
                --rotmat
